dd3566812556a7cb9769acd3e88fc2af484ebf3e
SalmanKatujuMengoding <131847037+MuhammadSalmanAnshariRizky@users.noreply.github.com>
2025-12-15 14:15:57 +0800

    perbaikan modal pada data evaluasi

--- resources/views/guru/dataaktivitas.blade.php
+++ resources/views/guru/dataaktivitas.blade.php
@@ -418,15 +418,11 @@
                                     Tutup
                                 </button>
                             </div>
-
-
-                            {{-- render semua modal yang di-push --}}
-                            @stack('modals')
                         </div>
                     </div>
                 </div>
+                @stack('modals')
 @endsection
-
             @push('styles')
                 <link rel="stylesheet" href="https://cdn.datatables.net/1.13.6/css/dataTables.bootstrap5.min.css">
                 <link rel="stylesheet" href="https://cdn.datatables.net/responsive/2.5.0/css/responsive.bootstrap5.min.css">
